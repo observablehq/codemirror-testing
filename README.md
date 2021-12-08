45b53a9c428bb5be89738071f163761e561e3347
Bryan Hughes <bryan@observablehq.com>
2021-12-08 15:11:42 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+CodeMirror Testing
+==================
+
+This repo exists to test out CodeMirror outside of the Observable infrastructure.
+The main use case is for quickly creating reproduction examples of bugs for
+reporting issues to CodeMirror.
+
+To keep things simple, we dispense with much of our normal tooling. We use
+TypeScript and Webpack directly to build the sample website, which only has
+CodeMirror dependencies in it (i.e. no Next.js, React, etc.)
+
+## Running this repo
+
+To use this repo, run the following:
+
+```
+npm install
+npm run build
+npm start
+```
+
+Then point your browser to http://localhost:8080/.
+
+You can also run `npm run watch` to have Webpack watch for changes. Note: this
+command does _not_ include hot reloading in the browser, so don't forget to
+manually reload after compilation finishes!
---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import {EditorState} from "@codemirror/state";
+import {EditorView, drawSelection, keymap} from "@codemirror/view";
+import {history, historyKeymap} from "@codemirror/history";
+import {defaultKeymap} from "@codemirror/commands";
+import {defaultHighlightStyle} from "@codemirror/highlight";
+import {lineNumbers} from "@codemirror/gutter";
+import {Text} from "@codemirror/text";
+import {html} from "@codemirror/lang-html";
+
+let lines = [`<!doctype html>`, `<meta charset="utf8">`, `<body>`];
+let repeated = [`  <p>These lines are repeated many times to save memory on`,
+                `  string data.</p>`,
+                `  <hr>`,
+                `  <img src="../logo.svg">`,
+                ``];
+for (let i = 0; lines.length < 100; i++) {
+  lines.push(repeated[i % repeated.length]);
+}
+lines.push(`</body>`, ``);
+
+const container = document.getElementById("editor")!;
+const views: EditorView[] = [];
+for (let i = 0; i < 100; i++) {
+  const div = document.createElement("div");
+  div.innerHTML = `Editor ${i + 1}`;
+  container.append(div);
+
+  const view = new EditorView({
+    state: EditorState.create({
+      doc: Text.of(lines),
+      extensions: [
+        keymap.of([...defaultKeymap, ...historyKeymap]),
+        history(),
+        drawSelection(),
+        defaultHighlightStyle,
+        lineNumbers(),
+        html()
+      ]
+    })
+  });
+  container.append(view.dom);
+  views.push(view);
+}
+
+let currentView = 0;
+function scrollintoView() {
+  const view = views[currentView];
+  document.getElementById("currentView")!.innerHTML = `Current editor = ${currentView}`
+  view.scrollPosIntoView(Math.floor(view.state.doc.length / 2));
+}
+
+document.getElementById("next")!.addEventListener("click", () => {
+  currentView++;
+  if (currentView === views.length) {
+    currentView = 0;
+  }
+  scrollintoView();
+});
+
+document.getElementById("previous")!.addEventListener("click", () => {
+  currentView--;
+  if (currentView < 0) {
+    currentView = views.length - 1;
+  }
+  scrollintoView();
+});
